b8efe4b9efe632033a6770e72d03a0d241b78deb
Quique <quiqueferre12@gmail.com>
2021-05-08 15:08:03 +0200

    Proyecto

---
+++ le_resort/WebApplication1/Home.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication1.localhost;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebService1 web = new WebService1();
+            DataTable dt = web.GetAllClients();
+
+          foreach(DataRow dr in dt.Rows)
+          {
+                TextBox1.Text = dr["idClient"].ToString();
+          }
+            
+            
+        }
+    }
+}
---
+++ le_resort/le_resort/WebService1.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Data;
+using System.Data.SQLite;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace le_resort
+{
+    /// <summary>
+    /// Summary description for WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+       
+        
+        public string DBpath = HttpRuntime.AppDomainAppPath + "database.db";
+
+        
+        [WebMethod]
+        public DataTable GetAllClients()
+        {
+            string DBpath = Server.MapPath("database.db");
+            DataTable dt = new DataTable();
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=" + DBpath + ";Version=3;"))
+            {
+                conn.Open();
+                SQLiteCommand comm = new SQLiteCommand("SELECT * FROM clients", conn);
+                SQLiteDataReader reader = comm.ExecuteReader();
+                dt.Load(reader);
+                conn.Close();
+            }
+            return dt;
+        }
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
